df58065a6986a9fa4d0260b06229ad85968123c5
Ader G <ader.guihannec@gmail.com>
2022-09-11 20:27:43 +0400

    separate v1 to v2

--- src/CalendlyApiV2.php
+++ src/CalendlyApiV2.php
@@ -2,19 +2,47 @@
 
 namespace Calendly;
 
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\RequestOptions;
-use JsonException;
-use Log;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\RequestOptions;
 
-class CalendlyApiV2 extends CalendlyApi
+class CalendlyApiV2
 {
+
+    /**
+     * @const string
+     */
+    public const EVENT_CREATED = 'invitee.created';
+
+    /**
+     * @const string
+     */
+    public const EVENT_CANCELED = 'invitee.canceled';
+
+    /**
+     * @const string
+     */
+    private const METHOD_GET = 'get';
+
+    /**
+     * @const string
+     */
+    private const METHOD_POST = 'post';
+
+    /**
+     * @const string
+     */
+    private const METHOD_DELETE = 'delete';
+
     /**
      * @const string
      */
     protected const API_URL = 'https://api.calendly.com';
+
+    /**
+     * @var Client
+     */
+    private $client;
 
     /**
      * @param string      $apiKey
@@ -22,7 +50,6 @@ class CalendlyApiV2 extends CalendlyApi
      */
     public function __construct($apiKeyV2, Client $client = null)
     {
-        parent::__construct($apiKeyV2, $client);
         $this->client = $client ?? new Client([
                 'base_uri' => self::API_URL,
                 'headers'  => [
@@ -41,7 +68,7 @@ class CalendlyApiV2 extends CalendlyApi
      *
      * @throws CalendlyApiException
      */
-    public function createWebhookV2($url, $events, $organizationUrl, $userUrl): array
+    public function createWebhook($url, $events, $organizationUrl, $userUrl): array
     {
         if (array_diff($events, [self::EVENT_CREATED, self::EVENT_CANCELED])) {
             throw new CalendlyApiException('The specified event types do not exist');
@@ -65,7 +92,7 @@ class CalendlyApiV2 extends CalendlyApi
      *
      * @throws CalendlyApiException
      */
-    public function getWebhookV2($id): array
+    public function getWebhook($id): array
     {
         return $this->callApi(self::METHOD_GET, 'webhook_subscriptions/' . $id);
     }
@@ -77,7 +104,7 @@ class CalendlyApiV2 extends CalendlyApi
      *
      * @throws CalendlyApiException
      */
-    public function getWebhooksV2(): array
+    public function getWebhooks(): array
     {
         return $this->callApi(self::METHOD_GET, 'webhook_subscriptions');
     }
@@ -112,4 +139,71 @@ class CalendlyApiV2 extends CalendlyApi
         }
     }
 
+    /**
+     * Test authentication token.
+     *
+     * @return array
+     *
+     * @throws CalendlyApiException
+     */
+    public function echo(): array
+    {
+        return $this->callApi(self::METHOD_GET, 'echo');
+    }
+
+
+    /**
+     * @param string $method
+     * @param string $endpoint
+     * @param array  $params
+     *
+     * @return array|null
+     *
+     * @throws CalendlyApiException
+     */
+    private function callApi($method, $endpoint, array $params = [])
+    {
+        $url = sprintf('/%s', $endpoint);
+
+        $data = [
+            RequestOptions::QUERY => $params,
+        ];
+
+        if ($method != self::METHOD_GET) {
+            $data = [
+                RequestOptions::JSON => $params,
+            ];
+        }
+
+        try {
+            try {
+                $response = $this->client->request($method, $url, $data);
+            } catch (GuzzleException $e) {
+                if ($e instanceof ClientException && $e->getResponse()) {
+                    $response = $e->getResponse();
+                    $message  = (string)$response->getBody();
+                    $headers  = $response->getHeader('content-type');
+
+                    if (count($headers) && strpos($headers[0], 'application/json') === 0) {
+                        $message = json_decode((string)$response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+                        $message = $message['message'];
+                    }
+
+                    throw new CalendlyApiException($message, $response->getStatusCode());
+                } else {
+                    throw new CalendlyApiException('Failed to get Calendly data: ' . $e->getMessage(), $e->getCode());
+                }
+            }
+
+            $headers = $response->getHeader('content-type');
+
+            if (count($headers) && strpos($headers[0], 'application/json') === 0) {
+                $response = json_decode((string)$response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+            }
+        } catch (JsonException $e) {
+            throw new CalendlyApiException('Invalid JSON: ' . $e->getMessage(), 500);
+        }
+
+        return $response;
+    }
 }
